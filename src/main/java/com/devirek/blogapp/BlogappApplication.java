a6f8e3c1b56998a1b6a0a8b28dd4a2baec5456cc
jaDevirek <wiciorny@gmail.com>
2020-06-18 13:47:47 +0200

    Initial commit, setup for project. Adding frameworks and libs

---
+++ src/main/java/com/devirek/blogapp/BlogappApplication.java
@@ -0,0 +1,15 @@
+package com.devirek.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogappApplication.class, args);
+        String
+                
+    }
+
+}
---
+++ src/test/java/com/devirek/blogapp/BlogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devirek.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
