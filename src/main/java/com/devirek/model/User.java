9ed7e5a6177c8ce048f550e7a6d9f59e93f5ea86
jaDevirek <wiciorny@gmail.com>
2020-06-20 17:00:57 +0200

    Adding Domain models classes

---
+++ src/main/java/com/devirek/model/User.java
@@ -0,0 +1,54 @@
+package com.devirek.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String userPassword;
+
+    @Column
+    private String userEmail;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
